7327d3c25fcd4a46d2d534907f7468c6e58726a1
cquarles14 <catlynne.quarles@sinclair.edu>
2022-04-11 23:20:54 -0400

    Initial Commit

---
+++ Source_Code_Management_Assn_2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Source_Code_Management_Assn_2
+//
+//  Created by Catlynne Quarles on 4/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
